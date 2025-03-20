ae6fb8cd2ab22f3728a703a9c6ce8fca4e897ef0
Rodvels <rodrigovelas2@gmail.com>
2025-03-20 00:20:12 -0500

    feat: Login

--- task/views.py
+++ task/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
@@ -11,6 +11,7 @@ from django.db import IntegrityError
 def home(request):
     # form se usa en HTML
     return render(request, 'home.html')
+
 
 def signup(request):
     """
@@ -54,10 +55,35 @@ def signup(request):
         })
 
 # Función para mostrar las tareas
+
+
 def tasks(request):
     return render(request, 'tasks.html')
 
 # Función para cerrar sesión
+
+
 def signout(request):
     logout(request)
     return redirect('home')
+
+
+# Función para iniciar sesión
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or Password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+
