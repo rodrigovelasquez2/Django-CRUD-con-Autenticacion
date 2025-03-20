3227a9862ae690e51e7a4c2aadd078dacd97ae54
Rodvels <rodrigovelas2@gmail.com>
2025-03-20 08:38:09 -0500

    feat: Configurar relaciones foraneas user one to many task en lado administrador y especificar valores de lectura como visualizar que tarea le corresponde a cada usuario

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Task
+
+"""
+Este módulo se encarga de registrar el modelo Task en el sitio de administración de Django.
+
+1. **Importación de módulos necesarios**:
+    - Se importa `admin` desde `django.contrib` para interactuar con el sistema de administración de Django.
+    - Se importa el modelo `Task` desde el archivo `models.py` del mismo módulo.
+
+2. **Descripción del propósito**:
+    - Al registrar el modelo `Task`, este se vuelve accesible y manejable desde la interfaz de administración de Django.
+    - Esto permite a los administradores realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) sobre las instancias de `Task`.
+
+3. **Definición de la clase `TaskAdmin`**:
+    - Se crea una clase personalizada `TaskAdmin` que hereda de `admin.ModelAdmin`.
+    - Se especifica el campo `readonly_fields` para que el campo `created` sea de solo lectura en el panel de administración.
+    - Se define `list_display` para mostrar los campos `title`, `created`, `dateCompleted`, `important` y `user` en la lista de objetos del modelo.
+
+4. **Registro del modelo**:
+    - Se registra el modelo `Task` junto con la clase personalizada `TaskAdmin` en el sitio de administración de Django.
+    - Esto asegura que las configuraciones definidas en `TaskAdmin` se apliquen al modelo `Task` en el panel de administración.
+"""
+
+class TaskAdmin(admin.ModelAdmin):
+     readonly_fields = ('created',)
+     list_display = ('title', 'created', 'dateCompleted', 'important', 'user')
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
--- task/models.py
+++ task/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    """
+    Modelo que representa una tarea en el sistema.
+    Atributos:
+        title (CharField): Título de la tarea con un límite máximo de 200 caracteres.
+        description (TextField): Descripción opcional de la tarea. Puede estar en blanco.
+        created (DateTimeField): Fecha y hora en que se creó la tarea. Se establece automáticamente al crearla.
+        dateCompleted (DateTimeField): Fecha y hora en que se completó la tarea. Puede ser nulo.
+        important (BooleanField): Indica si la tarea es importante. Por defecto, es False.
+        user (ForeignKey): Relación con el modelo User. Cada usuario puede tener múltiples tareas. 
+                           Si el usuario es eliminado, sus tareas asociadas también se eliminan.
+    """
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    dateCompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    
+    # Cada user tiene sus propias tareas (user to many task)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        """
+        Método que devuelve una representación en cadena del objeto Task.
+        Retorna:
+            str: Cadena que representa la tarea.
+            user: Muestra el usuario que tiene esa tarea
+        """
+        return self.title + '-' + str(self.user.username)
